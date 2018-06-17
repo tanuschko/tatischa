b8a5e82e2e324a3a0f224842b9d8e5c8df838d01
tanuschko <40324218+tanuschko@users.noreply.github.com>
2018-06-17 09:15:01 +0200

    initial commit

---
+++ src/main/kotlin/rsworld/eu/tatischa/TatischaApplication.kt
@@ -0,0 +1,11 @@
+package rsworld.eu.tatischa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TatischaApplication
+
+fun main(args: Array<String>) {
+	runApplication<TatischaApplication>(*args)
+}
